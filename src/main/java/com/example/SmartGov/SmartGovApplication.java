e802fb05a574191900b9d34f1abe909d6c33fa32
ASHISHVERMASH <160701608+ASHISHVERMASH@users.noreply.github.com>
2025-10-31 13:26:44 +0530

    Initial Spring Boot SmartGov project setup

---
+++ src/main/java/com/example/SmartGov/SmartGovApplication.java
@@ -0,0 +1,13 @@
+package com.example.SmartGov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartGovApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartGovApplication.class, args);
+	}
+
+}
