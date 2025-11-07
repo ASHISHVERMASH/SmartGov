c2e022f669fb701588d340f40389973b0e3d20b9
ASHISHVERMASH <160701608+ASHISHVERMASH@users.noreply.github.com>
2025-11-07 17:39:09 +0530

    .

---
+++ src/main/java/com/example/SmartGov/entity/Gender.java
@@ -0,0 +1,5 @@
+package com.example.SmartGov.entity;
+
+public enum Gender {
+    Male , Female , Other
+}
---
+++ src/main/java/com/example/SmartGov/entity/Role.java
@@ -0,0 +1,5 @@
+package com.example.SmartGov.entity;
+
+public enum Role {
+    ADMIN , CITIZEN , OFFICERS
+}
