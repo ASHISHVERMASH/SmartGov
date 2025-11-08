84c1c99d23a6d7217a24a2869f9eb773db6d5335
ASHISHVERMASH <160701608+ASHISHVERMASH@users.noreply.github.com>
2025-11-08 23:53:55 +0530

    .

--- src/main/java/com/example/SmartGov/dto/UserDto.java
+++ src/main/java/com/example/SmartGov/dto/UserDto.java
@@ -4,6 +4,8 @@ import com.example.SmartGov.enums.District;
 import com.example.SmartGov.enums.Gender;
 import com.example.SmartGov.enums.ROLES;
 import com.example.SmartGov.enums.State;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,18 +23,49 @@ public class UserDto {
 
     private Long userId;
 
-
+    @NotBlank(message = "First Name Cannot be blank")
     private String firstName;
+
+    @NotBlank(message = "Last Name Cannot be Blank")
     private String lastName;
+
+    @NotNull(message = "dateOfBirth Cannot be null")
+    @Past(message = "dateOfBirth must be in past")
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate dateOfBirth;
+
+    @NotBlank(message = "Aadhar Number  cannot be blank")
+    @Size(min =12 , max = 12, message = "Aadhar Number must be exactly 12 in digit ")
+    @Pattern(regexp = "^[0-9]{12}$" , message = "Aadhar Number must contain only digit ")
     private String aadharNumber;
+
+    @NotBlank(message = "Email Id cannot be blank ")
+    @Email(message = "Invalid Email Id")
     private String email;
+
+    @NotBlank(message = "Mobile  Nummber cannot be blank ")
+    @Size(min = 10, max = 10, message = "Mobile Number must be in 10 digit ")
+    @Pattern(regexp = "^[6-9][0-9]{9}$", message = "Mobile number must be valid")
     private String mobileNumber;
+
+    @NotNull(message = "State must be selected")
     private State state;
+
+    @NotNull(message = "District must be selected")
     private District district;
+
+    @NotNull(message = "Gender must be selected")
     private Gender gender;
+
+    @NotNull(message = "Roles must be selected")
     private ROLES role;
+
+    @PastOrPresent(message = "Created date cannot be in the future")
     private LocalDate createdAt;
+
+    @NotBlank(message = "Username cannot be blank")
+    @Size(min = 4, max = 20, message = "Username must be between 4 and 20 characters")
+    @Pattern(regexp = "^[A-Za-z0-9_]+$", message = "Username can only contain letters, numbers, and underscores")
     private String username;
     private String password;
 
